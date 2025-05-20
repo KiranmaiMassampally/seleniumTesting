81fc91e8f2f93a4871ead9c77e808ee2d0fd95d2
kiranmai <115510367+akira28-spec@users.noreply.github.com>
2025-05-20 16:47:43 -0500

    Your commit message here

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -2,26 +2,55 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import utils.ExtentReportManager;
+import utils.Log;
 
 public class BaseTest {
   protected WebDriver driver;
+  protected static ExtentReports extent;
+  protected ExtentTest test;
+  
+  @BeforeSuite  
+  public void SetupReport() {
+	  extent = ExtentReportManager.getReportsInstance();
+  }
+  @AfterSuite
+  public void TeardownReport() {
+	 extent.flush();
+  }
   
  @BeforeMethod
   public void SetUp() {
+	 Log.info("Settingup the WedDriver...");
 	 WebDriverManager.chromedriver().setup(); // Automatically handles driver setup
      driver = new ChromeDriver();
      driver.manage().window().maximize();
+     Log.info("Navigating to URL...");
      driver.get("https://admin-demo.nopcommerce.com/login");
   }
  
  @AfterMethod
- public void tearDown() {
+ public void tearDown(ITestResult result) {
+	 if(result.getStatus()== ITestResult.FAILURE) {
+		 String screenshotPath = ExtentReportManager.captureScreenShot(driver, "LoginFailure");
+		 test.fail("test failed check screenshot", 
+				 MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+	 }
+	 
+	 Log.info("closing browser...");
 	 if( driver!=null) {
-		// driver.quit();
+		 driver.quit();
 	 }
  }
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -5,16 +5,59 @@ import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
+import utils.ExtentReportManager;
+import utils.Log;
 @Test
 public class LoginTest extends BaseTest{
 		//only test scripts here
-	public void TestValidLogin() {
+
+public void TestValidLogin() {
+		
+		Log.info("Starting logging test...");
+		test = ExtentReportManager.createTest("LoginTests");
 		LoginPage loginPage = new LoginPage(driver);
+		test.info("Navigating to the page");
+		
+		
+		Log.info("Adding the credentialss...");
+		test.info("adding credentials");
+		
 		loginPage.enterUserName("admin@yourstore.com");
 		loginPage.enterPassword("admin");
+		test.info("Clicking on login Button");
 		loginPage.clickLogin();
+		
 		System.out.println("title of page:"+driver.getTitle());
+		Log.info("Verifying title...");
+		test.info("Verifying page title");
 		Assert.assertEquals(driver.getTitle(),"Just a moment...");
+		test.info("Login successfull");
+		
 	}
+@Test
+public void TestLoginWithWrongDetails() {
+	
+	Log.info("Starting logging test...");
+	test = ExtentReportManager.createTest("LoginTests with invalid Creds");
+	LoginPage loginPage = new LoginPage(driver);
+	test.info("Navigating to the page");
+	
+	
+	Log.info("Adding the credentialss...");
+	test.info("adding credentials");
+	
+	loginPage.enterUserName("test@yourstore.com");
+	loginPage.enterPassword("test");
+	test.info("Clicking on login Button");
+	loginPage.clickLogin();
+	
+	System.out.println("title of page:"+driver.getTitle());
+	Log.info("Verifying title...");
+	test.info("Verifying page title");
+	Assert.assertEquals(driver.getTitle(),"Just a moment...");
+	test.info("Login successfull");
+	
+}
 
 }
+ 
